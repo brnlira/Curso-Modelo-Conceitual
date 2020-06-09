b0c1fe78ba1b329c4c4e7f2353f4462ab887f312
brnlira <52218318+brnlira@users.noreply.github.com>
2020-06-09 12:35:55 -0300

    Validação customizada: email não repetido na atualização de cliente

--- src/main/java/com/brunomarqueslirainformatica/cursoSpringExercicio1/dto/ClienteDTO.java
+++ src/main/java/com/brunomarqueslirainformatica/cursoSpringExercicio1/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.brunomarqueslirainformatica.cursoSpringExercicio1.domain.Cliente;
+import com.brunomarqueslirainformatica.cursoSpringExercicio1.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/brunomarqueslirainformatica/cursoSpringExercicio1/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/brunomarqueslirainformatica/cursoSpringExercicio1/services/validation/ClienteUpdateValidator.java
@@ -2,43 +2,44 @@ package com.brunomarqueslirainformatica.cursoSpringExercicio1.services.validatio
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.brunomarqueslirainformatica.cursoSpringExercicio1.domain.Cliente;
-import com.brunomarqueslirainformatica.cursoSpringExercicio1.domain.enums.TipoCliente;
-import com.brunomarqueslirainformatica.cursoSpringExercicio1.dto.ClienteNewDTO;
+import com.brunomarqueslirainformatica.cursoSpringExercicio1.dto.ClienteDTO;
 import com.brunomarqueslirainformatica.cursoSpringExercicio1.repositories.ClienteRepository;
 import com.brunomarqueslirainformatica.cursoSpringExercicio1.resources.exception.FieldMessage;
-import com.brunomarqueslirainformatica.cursoSpringExercicio1.services.validation.utils.BR;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	HttpServletRequest request;
 
 	@Autowired
 	private ClienteRepository repo;
 
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
 
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
 		List<FieldMessage> list = new ArrayList<FieldMessage>();
 
-		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getcpfouCnpj())) {
-			list.add(new FieldMessage("cpfouCnpj", "CPF inválido"));
-		} 
-		
-		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getcpfouCnpj())) {
-			list.add(new FieldMessage("cpfouCnpj", "CNPJ inválido"));
-		} 
-		
 		Cliente aux = repo.findByEmail(objDto.getEmail());
-		if(aux != null) {
+		if (aux != null && !aux.equals(uriId)) {
 			list.add(new FieldMessage("email", "Email já cadastrado na base"));
 		}
 
